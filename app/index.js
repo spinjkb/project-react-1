bfc6360c58f408ccd2a2a53c2561f62d58eb3e96
jkbspin <960713393@qq.com>
2017-04-01 23:38:11 +0800

    使用 webpack.config.js 和 npm scripts

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
+import j from 'jquery'
 function component () {
-  var element = document.createElement('div');
-
+  //var element = document.createElement('div');
+  var element = j('<div></div>');
   /* lodash is required for the next line to work */
   /*_.join，这个 _ 实际上是 lodash 暴露的全局变量。*/ 
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+  //element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.html(_.join(['Hello','webpack'], ' '))
+  //return element;
+  return element.get(0);
 }
-
 document.body.appendChild(component());
+// console.log(1)
