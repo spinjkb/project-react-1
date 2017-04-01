65571c76f30fca89f02107598ba0e7f05905862f
jkbspin <960713393@qq.com>
2017-04-01 23:48:13 +0800

    添加foo.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import j from 'jquery'
+import j from 'jquery';
+import foo from './foo'
 function component () {
   //var element = document.createElement('div');
   var element = j('<div></div>');
@@ -12,3 +13,5 @@ function component () {
 }
 document.body.appendChild(component());
 // console.log(1)
+console.log(foo)
+console.log(foo())
