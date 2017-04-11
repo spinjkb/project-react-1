9d9805336d049cdb164f146372c3dce29fd0b10f
jkbspin <960713393@qq.com>
2017-04-11 23:36:05 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -3,23 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import Welcome from './Welcome';
-// ReactDOM.render(
-//   <h1>Hi, world!</h1>,
-//   document.getElementById('root')
-// );
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-//   );
-// }
-// setInterval(tick, 1000);
+
 
 ReactDOM.render(
   <Welcome name="jkb"/>,
